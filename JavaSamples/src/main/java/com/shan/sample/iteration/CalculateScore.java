9a9da24f293033fd41900a7a498d38301a06ea66
Shan <srikishan2000@gmail.com>
2018-11-30 22:35:20 +0200

    organized imports

--- JavaSamples/src/main/java/com/shan/sample/iteration/CalculateScore.java
+++ JavaSamples/src/main/java/com/shan/sample/iteration/CalculateScore.java
@@ -1,8 +1,6 @@
 package com.shan.sample.iteration;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Iterator;
 import java.util.List;
 import java.util.stream.Collectors;
 
