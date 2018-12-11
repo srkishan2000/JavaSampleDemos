5db40e85559d2a78cbc4140c93dcb825dbbe4ef9
Shan-Unikie <shanmuga.ganesan@unikie.com>
2018-12-11 21:47:44 +0200

    Data type - changed minor changes

--- JavaSamples/src/main/java/com/shan/sample/secondsandminuteschallenge/SecondsAndMinutesChallenge.java
+++ JavaSamples/src/main/java/com/shan/sample/secondsandminuteschallenge/SecondsAndMinutesChallenge.java
@@ -32,26 +32,26 @@ package com.shan.sample.secondsandminuteschallenge;
 
 public class SecondsAndMinutesChallenge {
 
-	public static String getDurationString (int minutes, int seconds) {
+	public static String getDurationString (long minutes, long seconds) {
 		
-		if ( minutes < 0 && (seconds < 0 || seconds > 59)) {
+		if ( minutes < 0 || (seconds < 0 || seconds > 59)) {
 			return "Invalid Value ...";
 		}
 		
-		int hours = minutes / 60;
-		int remainingMinutes = minutes % 60;
+		long hours = minutes / 60;
+		long remainingMinutes = minutes % 60;
 		
 		return  hours + "h " + remainingMinutes + "m " + seconds + "s";
 		
 	}
 	
-	public static String getDurationString (int seconds) {
+	public static String getDurationString (long seconds) {
 		if ( seconds < 0 ) {
 			return "Invalid Value ...";
 		}
 		
-		int minutes = seconds / 60;
-		int remainingSeconds = seconds % 60;
+		long minutes = seconds / 60;
+		long remainingSeconds = seconds % 60;
 		
 		return getDurationString(minutes, remainingSeconds);
 	}
