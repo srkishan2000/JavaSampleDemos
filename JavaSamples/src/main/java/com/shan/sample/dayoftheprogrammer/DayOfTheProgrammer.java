6f0c5f6d8e0e0977c8fd5426985c32c7ac7897df
Shan <srikishan2000@gmail.com>
2019-02-19 14:30:21 +0200

    added new sample

---
+++ JavaSamples/src/main/java/com/shan/sample/dayoftheprogrammer/DayOfTheProgrammer.java
@@ -0,0 +1,45 @@
+package com.shan.sample.dayoftheprogrammer;
+
+import java.time.Year;
+import java.time.format.DateTimeFormatter;
+
+public class DayOfTheProgrammer {
+	
+	private static int DAY_OF_THE_PROGRAMMER = 256;
+	private static int YEAR = 1820;  // 2016  1800
+
+	public static void main(String[] args) {
+		System.out.println(DAY_OF_THE_PROGRAMMER);
+		
+		if (YEAR < 1700  || YEAR > 2700) {
+			System.out.println("INVALID RANGE");
+		} else {
+//			DateTimeFormatter dtfm = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+			
+			
+		    if (YEAR == 1918)
+		    	System.out.println( "26.09." + YEAR);
+		        		
+		    if (YEAR % 4 == 0 && (YEAR < 1918 || YEAR % 400 == 0 || YEAR % 100 != 0)) {
+		    	System.out.println( "12.09." + YEAR);
+		    } else {
+		    	System.out.println( "13.09." + YEAR);
+		    }
+			
+			
+//			// cheking leap year or not
+//			if (Year.isLeap(YEAR)) {
+//				System.out.println("it is a leap year");
+//			}
+//			
+//			
+//			System.out.println(DAY_OF_THE_PROGRAMMER);
+//			System.out.println(Year.of(YEAR).atDay(DAY_OF_THE_PROGRAMMER).format(dtfm));	
+		}
+		
+		
+		
+		
+	}
+
+}
