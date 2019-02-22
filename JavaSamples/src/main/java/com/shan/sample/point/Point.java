3ffc468cf18c528f61524565f2aeaeddb34191b9
Shan <srikishan2000@gmail.com>
2019-02-22 07:13:18 +0200

    Optimized

--- JavaSamples/src/main/java/com/shan/sample/point/Point.java
+++ JavaSamples/src/main/java/com/shan/sample/point/Point.java
@@ -65,15 +65,12 @@ public class Point {
 	private int y;
 	
 	public Point() {
-		this.x = 0;
-		this.y = 0;
 	}
 
 	public Point(int x, int y) {
 		this.x = x;
 		this.y = y;
 	}
-	
 	
 	public int getX() {
 		return x;
@@ -96,7 +93,7 @@ public class Point {
 	}
 	
 	public double distance(int x, int y) {
-		return Math.sqrt(((x - getX())*(x - getX())) + ((y-getY())*(y-getY())));
+		return Math.sqrt(Math.pow(x - getX(), 2) + Math.pow(y - getY(), 2));
 	}
 	
 	public double distance(Point p) {
